f425d18d40372c9022e249cc839c9fc1f70e5729
Rene Franco <reneafranco@gmail.com>
2024-07-21 21:49:00 +0000

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import  MagicButton  from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -10,16 +13,40 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
         </div>
         <div>
-            <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-                {/* Radial gradient for the container to give a faded look */}
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-                    <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-                        Backgrounds
-                    </p>
-                </div>
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.03] bg-dot-black/[0.2] flex items-center justify-centerMagicButton; absolute top-0 left-0">
+               <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>                
+            </div>
+            <div className='flex justify-center relative y-20 z-10'>
+              <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col
+            items-center justify-center' >
+                <h2 className='uppercase tracking-widest
+              text-xs text-center text-blue-100 max-w-80'>
+                  Dynamy Web magic with Next.js
+                </h2>
+
+                <TextGenerateEffect 
+                  className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                  words="Trasnforming Concepts into Seamless Experiences"
+                />
+                
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                    Hi, I'm Rene, a full stack developer base in United States
+                </p>
+
+                <a>
+                  <MagicButton 
+                  title='Show my work'
+                  icon={< FaLocationArrow/>}
+                  position='right'
+                  />
+                </a>
+              </div> 
+              
+          </div>
         </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
+
