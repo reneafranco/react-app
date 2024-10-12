9d511fd5e1af167ef66043e851f06cc1eab101a9
reneafranco <reneadonayfranco@gmail.com>
2024-10-12 14:04:15 -0400

    stats ok

--- components/ui/Stats.tsx
+++ components/ui/Stats.tsx
@@ -4,48 +4,60 @@ import { Cousine } from "next/font/google";
 import CountUp from "react-countup";
 
 const stats = [
-    {
-        num: 5,
-        text: "Years of experience",
-    },
-    {
-        num: 26,
-        text: "Projects completed",
-    },
-    {
-        num: 10,
-        text: "Technologies mastered",
-    },
-    {
-        num: 1010,
-        text: "Code commits",
-    }
-]
+  {
+    num: 5,
+    text: "Years of experience",
+  },
+  {
+    num: 26,
+    text: "Projects completed",
+  },
+  {
+    num: 10,
+    text: "Technologies mastered",
+  },
+  {
+    num: 996,
+    text: "Code commits",
+  },
+];
 
 const Stats = () => {
   return (
     <section>
-    <div className="container mx-auto pb-20">
-      <div className="flex flex-wrap gap-6 max-w-[80vw] 
-        mx-auto xl:max-w-none">
-      {stats.map((item, index) => {
-        return <div className="flex-1 flex gap-4 items-center
-          justify-center xl:justify-start" key={ index }>
-          <CountUp end={item.num} duration={5} delay={2}
-            className="text-4xl xl:text-6xl font-extrabold"/>
-          <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>
-            {item.text}
-          </p>
+      <div className="container mx-auto pb-20">
+        <div
+          className="flex flex-wrap gap-6 max-w-[80vw] 
+        mx-auto xl:max-w-none"
+        >
+          {stats.map((item, index) => {
+            return (
+              <div
+                className="flex-1 flex gap-4 items-center
+          justify-center xl:justify-start"
+                key={index}
+              >
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-extrabold text-white"
+                />
+                <p
+                  className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
         </div>
-      })}
       </div>
-    </div>
     </section>
-  )
-}
+  );
+};
 
-export default Stats
-
+export default Stats;
 
 // "use client";
 
@@ -64,7 +76,7 @@ export default Stats
 //     const fetchGitHubData = async () => {
 //       try {
 //         const username = 'reneafranco'; // Reemplaza con tu nombre de usuario de GitHub
-        
+
 //         // Llama a la API para obtener datos del usuario
 //         const userResponse = await fetch(`https://api.github.com/users/${username}`);
 //         const userData = await userResponse.json();
@@ -72,11 +84,11 @@ export default Stats
 //         // Llama a la API para obtener los repositorios
 //         const reposResponse = await fetch(`https://api.github.com/users/${username}/repos?per_page=1`);
 //         const reposData = await reposResponse.json();
-        
+
 //         // Log para verificar los datos obtenidos
 //         console.log('User Data:', userData);
 //         console.log('Repos Data:', reposData);
-        
+
 //         // Cambia los datos de stats según los datos de la API
 //         setStats([
 //           { num: 0, text: "Years of experience" }, // Placeholder para los años de experiencia
@@ -111,4 +123,3 @@ export default Stats
 // }
 
 // export default Stats;
-
