b62475d278b623d2436106ce465282e54db89931
reneafranco <reneadonayfranco@gmail.com>
2024-11-20 20:19:12 -0500

    first row ok

--- components/ui/InfinitiSliderBasic.tsx
+++ components/ui/InfinitiSliderBasic.tsx
@@ -14,22 +14,22 @@ export function InfiniteSliderBasic() {
         className="aspect-square w-[120px] rounded-[4px]"
       />
       <img
-        src="https://i.scdn.co/image/ab67616d00001e02ecdb8f824367a53468100faf"
-        alt="Yung Lean - Stardust"
+        src="/assets/spring.svg"
+        alt="Spring Boot - Backend Frameork"
         className="aspect-square w-[120px] rounded-[4px]"
       />
       <img
-        src="https://i.scdn.co/image/ab67616d00001e021624590458126fc8b8c64c2f"
-        alt="Lana Del Rey - Ultraviolence"
+        src="/assets/python.svg"
+        alt="Python - Programing Languaje"
         className="aspect-square w-[120px] rounded-[4px]"
       />
       <img
-        src="https://i.scdn.co/image/ab67616d00001e020dcf0f3680cff56fe5ff2288"
-        alt="A$AP Rocky - Tailor Swif"
+        src="/assets/java.svg"
+        alt="Java - Programing Languaje"
         className="aspect-square w-[120px] rounded-[4px]"
       />
       <img
-        src="https://i.scdn.co/image/ab67616d00001e02bc1028b7e9cd2b17c770a520"
+        src="/assets/typeScript.svg"
         alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
         className="aspect-square w-[120px] rounded-[4px]"
       />{" "}
