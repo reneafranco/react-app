1d341eabfd8c6d673f13028483289323d551dc07
reneafranco <reneadonayfranco@gmail.com>
2024-11-25 10:31:01 -0500

    icons-ok

--- components/ui/InfinitiSliderBasic.tsx
+++ components/ui/InfinitiSliderBasic.tsx
@@ -1,37 +1,64 @@
+import Image from "next/image";
 import { InfiniteSlider } from "./InfinityAnimation";
 
 export function InfiniteSliderBasic() {
   return (
     <InfiniteSlider gap={24} reverse>
-      <img
-        src="/assets/arch.svg"
-        alt="Arch Linux OS"
-        className="aspect-square w-[120px] rounded-[4px]"
-      />
-      <img
-        src="/assets/aws.svg"
-        alt="Amazon Web Services"
-        className="aspect-square w-[120px] rounded-[4px]"
-      />
-      <img
-        src="/assets/spring.svg"
-        alt="Spring Boot - Backend Frameork"
-        className="aspect-square w-[120px] rounded-[4px]"
-      />
-      <img
-        src="/assets/python.svg"
-        alt="Python - Programing Languaje"
-        className="aspect-square w-[120px] rounded-[4px]"
-      />
-      <img
-        src="/assets/java.svg"
-        alt="Java - Programing Languaje"
-        className="aspect-square w-[120px] rounded-[4px]"
-      />
-      <img
-        src="/assets/typeScript.svg"
-        alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
-        className="aspect-square w-[120px] rounded-[4px]"
+      <Image
+        src="/assets/icons/spring-icon.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/python.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/java.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/docker-icon.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/postgresql.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/git-icon.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/archlinux.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
+      />{" "}
+      <Image
+        src="/assets/icons/aws.svg"
+        alt="Dean Blunt - Black Metal 2"
+        width={120}
+        height={120}
+        className="aspect-square rounded-[4px]"
       />{" "}
     </InfiniteSlider>
   );
